2e21fe960dbfc751d5816a3c91f136de32f119f4
m5151128 <m5151129@gmail.com>
2014-05-12 14:47:41 +0900

    スプレットシート用テスト

---
+++ spreadsheet.php
@@ -0,0 +1,29 @@
+<?php
+  set_include_path('/home/nagayama/ZendTest/ZendGdata/library/');
+
+  require_once 'Zend/Loader.php';
+  Zend_Loader::loadClass('Zend_Gdata');
+  Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+  Zend_Loader::loadClass('Zend_Gdata_Spreadsheets');
+
+  // 認証準備
+  $email    = "hogehoge@gmail.com"; // メールアドレス
+  $password = "password"; // パスワード
+  $service  = Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
+
+  $client = Zend_Gdata_ClientLogin::getHttpClient($email, $password, $service);
+  $spreadsheet = new Zend_Gdata_Spreadsheets($client);
+
+  $spreadsheetKey = "spreadsheetKey"; // スプレットシートのキー
+  $worksheetId    = "od6"; // ワークシートのID
+
+  // 書き込みたいデータ(１行ぶん。列名=>データ)
+  $rowData = [
+      'col1' => 'test',
+      'col2' => 'テスト',
+      'col3' => date('Y/m/d'),
+  ];
+
+  // 1行ぶんのデータを最後尾に追記する
+  echo ($spreadsheet->insertRow($rowData, $spreadsheetKey, $worksheetId)) ? "OK" : "NG";
+  echo PHP_EOL;
