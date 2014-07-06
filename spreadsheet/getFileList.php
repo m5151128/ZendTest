4c45c4b6fee7e68308efcf5eba8d6541ad9fa1ac
m5151128 <m5151128@gmail.com>
2014-07-06 16:50:26 +0900

    対象ユーザのssファイルリストを取得する

---
+++ spreadsheet/getFileList.php
@@ -0,0 +1,44 @@
+<?php
+set_include_path(__DIR__ . '/../ZendGdata/library/');
+
+require_once 'Zend/Loader.php';
+Zend_Loader::loadClass('Zend_Gdata');
+Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+Zend_Loader::loadClass('Zend_Gdata_Spreadsheets');
+
+$email = getInput("メールアドレス(example: hogehoge@gmail.com)");
+$password = getInput("パスワード");
+
+function getInput($text)
+{
+    echo $text . ': ';
+    return trim(fgets(STDIN));
+}
+
+$sample = new getFileList($email, $password);
+$sample->run();
+
+class getFileList
+{
+    public function __construct($email, $password)
+    {
+        try {
+          $client = Zend_Gdata_ClientLogin::getHttpClient($email, $password, Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME);
+        } catch (Exception $e) {
+          exit("Error: メールアドレスまたはパスワードが正しくありません\n");
+        }
+
+        $spredsheetService = new Zend_Gdata_Spreadsheets($client);
+    }
+
+    public function run()
+    {
+        $feed = $spreadsheetService->getSpreadsheetFeed();
+
+        $i = 0;
+        foreach($feed->entries as $entry) {
+            echo $i . ': ' .  $entry->title . "\n";
+            $i++;
+        }
+    }
+}
